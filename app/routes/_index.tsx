fee3cdd76b9a73f316fccf1fe40004901da1a806
walabo-hy <y.hikaru@walabo.com>
2024-02-07 02:19:18 +0900

    Add API endpoint for test environment and update index page to display response and test environment

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,15 +13,20 @@ export const meta: MetaFunction = () => {
 export const loader: LoaderFunction = async ({ request }) => {
   const { protocol, host } = new URL(request.url)
   const res = await fetch(`${protocol}//${host}/api/helloworld`)
-  return json(await res.json())
+  const res2 = await fetch(`${protocol}//${host}/api/test_env`)
+  return json({
+    response: await res.json(),
+    test_env: await res2.json()
+  })
 }
 
 export default function Index() {
-  const data = useLoaderData()
+  const data = useLoaderData<any>()
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
-      <p>This is Response: {JSON.stringify(data)}</p>
+      <p>This is Response: {JSON.stringify(data.response)}</p>
+      <p>This is TEST ENV: {JSON.stringify(data.test_env)}</p>
     </div>
   );
 }
--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,11 +1,17 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/cloudflare-pages'
 
-const app = new Hono()
+const app = new Hono<any>()
 
 app.get('/api/helloworld', (c) => {
   return c.json({
     message: 'hello hono',
+  })
+})
+
+app.get('/api/test_env', (c) => {
+  return c.json({
+    message: c.env.TEST_ENV,
   })
 })
 
